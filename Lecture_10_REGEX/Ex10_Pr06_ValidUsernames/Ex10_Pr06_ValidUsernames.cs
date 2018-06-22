cde218079833107333352c75f028ca94d8af27c8
AndreyKo <andrey.kodzhabashev@gmail.com>
2018-06-22 19:48:11 +0300

    REGEX Solved problem 06

---
+++ Lecture_10_REGEX/Ex10_Pr06_ValidUsernames/Ex10_Pr06_ValidUsernames.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Ex10_Pr06_ValidUsernames
+{
+    class Ex10_Pr06_ValidUsernames
+    {
+        // 100/100
+        static void Main()
+        {
+            string validNamesPattern = @"\b[A-Za-z]\w{2,24}\b";
+
+            string inputText = Console.ReadLine();
+
+            Regex regex = new Regex(validNamesPattern);
+
+            MatchCollection valids = regex.Matches(inputText);
+
+            List<string> listNames = valids.Select(x => x.Value.ToString()).ToList();
+
+            List<KeyValuePair<string, int>> calculatedNames = new List<KeyValuePair<string, int>>();
+
+            for (int i = 0; i < listNames.Count - 1; i++)
+            {
+
+                var name1 = listNames[i];
+                var name2 = listNames[i + 1];
+
+                int result = name1.Length + name2.Length;
+
+                var concat = name1 + ":" + name2;
+
+                KeyValuePair<string, int> temp = new KeyValuePair<string, int>(concat, result);
+
+                calculatedNames.Add(temp);
+            }
+
+            var test = calculatedNames.OrderByDescending(c => c.Value).First();
+
+            var pairNames = test.Key.ToString().Split(":");
+
+            string first = pairNames[0];
+            string second = pairNames[1];
+
+
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+
+        }
+    }
+}
