63faaf90d420b124eb5d14921d5af1bd69986884
AndreyKo <andrey.kodzhabashev@gmail.com>
2018-06-22 19:23:09 +0300

    Exercises REGEX
    Signed-off-by: AndreyKo <andrey.kodzhabashev@gmail.com>

---
+++ Lecture_10_REGEX/EX10_Pr03_CameraView_REGEX/EX10_Pr03_CameraView_REGEX.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EX10_Pr03_CameraView_REGEX
+{
+    class EX10_Pr03_CameraView_REGEX
+    {
+        // 100/100
+        static void Main()
+        {
+            int[] tokens = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int skip = tokens[0];
+            int take = tokens[1];
+            string pattern = @"(?:\|<)(.*?(?=[\|]))|(?:\|<)(.*$)";
+            Regex regex = new Regex(pattern);
+
+            string inputText = Console.ReadLine();
+
+            MatchCollection substrings = regex.Matches(inputText);
+
+            List<string> test = new List<string>();
+
+            foreach (Match item in substrings)
+            {
+                test.Add(item.Value.ToString());
+
+            }
+
+            List<string> processed = new List<string>();
+
+            foreach (var item in test)
+            {
+                var temp = string.Join("", item.Skip(skip + 2).Take(take));
+                processed.Add(temp);
+            }
+
+            Console.WriteLine(string.Join(", ", processed));
+
+        }
+    }
+}
