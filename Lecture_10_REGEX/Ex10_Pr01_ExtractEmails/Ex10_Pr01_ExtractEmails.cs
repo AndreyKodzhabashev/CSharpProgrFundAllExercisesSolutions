2f9d7b0d2b4d9b7d494d37c7c320b1473c6dfa3c
AndreyKo <andrey.kodzhabashev@gmail.com>
2018-06-22 20:12:42 +0300

    Update comment about result

--- Lecture_10_REGEX/Ex10_Pr01_ExtractEmails/Ex10_Pr01_ExtractEmails.cs
+++ Lecture_10_REGEX/Ex10_Pr01_ExtractEmails/Ex10_Pr01_ExtractEmails.cs
@@ -5,6 +5,7 @@ namespace Ex10_Pr01_ExtractEmails
 {
     class Ex10_Pr01_ExtractEmails
     {
+        // 75/100 - dont know how to eliminate match, when starts with dot(.)
         static void Main()
         {
             Regex regex = new Regex(@"^|\b[A-Za-z]+(?<=)[\.-]*[A-Za-z]+@[A-Za-z]([A-Za-z\.-]+)\.[A-Za-z]+");
