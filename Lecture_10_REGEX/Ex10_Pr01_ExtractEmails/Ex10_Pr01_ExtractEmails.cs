2c678f9c73e600a494914301699dd6822b3caed5
AndreyKo <andrey.kodzhabashev@gmail.com>
2018-06-22 20:03:59 +0300

    75/100

---
+++ Lecture_10_REGEX/Ex10_Pr01_ExtractEmails/Ex10_Pr01_ExtractEmails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Ex10_Pr01_ExtractEmails
+{
+    class Ex10_Pr01_ExtractEmails
+    {
+        static void Main()
+        {
+            Regex regex = new Regex(@"^|\b[A-Za-z]+(?<=)[\.-]*[A-Za-z]+@[A-Za-z]([A-Za-z\.-]+)\.[A-Za-z]+");
+
+            string text = Console.ReadLine();
+
+            MatchCollection emails = regex.Matches(text);
+
+            foreach (Match item in emails)
+            {
+                Console.WriteLine(item.Value.ToString());
+            }
+        }
+    }
+}
