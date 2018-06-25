2b345cc475252657bd568e88700f71a76ef70956
AndreyKo <andrey.kodzhabashev@gmail.com>
2018-06-25 19:03:49 +0300

    SnowBall with Class
    Signed-off-by: AndreyKo <andrey.kodzhabashev@gmail.com>

---
+++ 1 Exam solutions/_05_Jan_2018_Pr01_Snowballs_var2/SnowBall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace _05_Jan_2018_Pr01_Snowballs_var2
+{
+    //class SnowBall
+    //{
+    //    public SnowBall(int snow, int time, int quallity, int result)
+    //    {
+    //        Snow = snow;
+    //        Time = time;
+    //        Quallity = quallity;
+    //        Result = result;
+    //    }
+
+    //    public int Snow { get; set; }
+    //    public int Time { get; set; }
+    //    public int Quallity { get; set; }
+
+    //    public int Result { get; set; }
+    //}
+}
---
+++ 1 Exam solutions/_05_Jan_2018_Pr01_Snowballs_var2/_05_Jan_2018_Pr01_Snowballs_var2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _05_Jan_2018_Pr01_Snowballs_var2
+{
+    class _05_Jan_2018_Pr01_Snowballs_var2
+    {
+        static void Main()
+        {
+
+            BigInteger ballCount = BigInteger.Parse(Console.ReadLine());
+
+            List<SnowBall> listBalls = new List<SnowBall>();
+
+            for (BigInteger i = 0; i < ballCount; i++)
+            {
+                BigInteger snow = BigInteger.Parse(Console.ReadLine());
+                BigInteger time = BigInteger.Parse(Console.ReadLine());
+                BigInteger quallity = BigInteger.Parse(Console.ReadLine());
+
+                BigInteger result = (BigInteger)BigInteger.Pow((snow / time), (int)quallity);
+
+                SnowBall currentBall = new SnowBall( snow, time, quallity, result);
+
+                listBalls.Add(currentBall);
+            }
+
+            foreach (var ball in listBalls.OrderByDescending(x =>x.Result))
+            {
+
+                Console.WriteLine($"{ball.Snow} : {ball.Time} = {ball.Result} ({ball.Quallity})");
+                break;
+
+            }
+
+        }
+    }
+    class SnowBall
+    {
+        public SnowBall(BigInteger snow, BigInteger time, BigInteger quallity, BigInteger result)
+        {
+            Snow = snow;
+            Time = time;
+            Quallity = quallity;
+            Result = result;
+        }
+
+        public BigInteger Snow { get; set; }
+        public BigInteger Time { get; set; }
+        public BigInteger Quallity { get; set; }
+
+        public BigInteger Result { get; set; }
+    }
+}
