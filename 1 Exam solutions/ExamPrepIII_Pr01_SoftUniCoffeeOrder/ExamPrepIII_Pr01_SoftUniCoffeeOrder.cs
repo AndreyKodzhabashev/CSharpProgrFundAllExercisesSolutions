10a13614f576fff7212535b62dee8d945ce51fb9
AndreyKo <andrey.kodzhabashev@gmail.com>
2018-06-26 12:55:13 +0300

    100/100 not commented, its easy to be understand
    Signed-off-by: AndreyKo <andrey.kodzhabashev@gmail.com>

---
+++ 1 Exam solutions/ExamPrepIII_Pr01_SoftUniCoffeeOrder/ExamPrepIII_Pr01_SoftUniCoffeeOrder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+namespace ExamPrepIII_Pr01_SoftUniCoffeeOrder
+{
+    class ExamPrepIII_Pr01_SoftUniCoffeeOrder
+    {
+        // 100/100
+        static void Main()
+        {
+            
+            int ordersCount = int.Parse(Console.ReadLine());
+
+            decimal totalPrice = 0;
+
+            for (int i = 0; i < ordersCount; i++)
+            {
+
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+
+                DateTime purchaseDate = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+
+                long capsulesCount = long.Parse(Console.ReadLine());
+                
+                int daysInMonth = DateTime.DaysInMonth(purchaseDate.Year, purchaseDate.Month);
+                                
+            decimal pricePerOrder = (daysInMonth * capsulesCount) * pricePerCapsule;
+
+
+                Console.WriteLine($"The price for the coffee is: ${pricePerOrder:F2}");
+
+                totalPrice += pricePerOrder;
+
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+
+        }
+    }
+}
